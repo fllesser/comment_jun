690ab488bf3d1850c8063532f323a8cb1d59ce37
YiJiuChow <19550518zrf@gmail.com>
2022-11-05 18:33:54 +0800

    synchronized (userId.toString().intern()) 悲观锁保证一人一单, 使用intern()保证只有一个锁, aopcontext获取代理对象, 防止this调用方法导致事务失效

--- src/main/java/com/cjun/CommentJunApplication.java
+++ src/main/java/com/cjun/CommentJunApplication.java
@@ -3,7 +3,9 @@ package com.cjun;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
+@EnableAspectJAutoProxy(exposeProxy = true) // 暴露代理对象
 @MapperScan("com.cjun.mapper")
 @SpringBootApplication
 public class CommentJunApplication {
--- src/main/java/com/cjun/service/impl/VoucherOrderServiceImpl.java
+++ src/main/java/com/cjun/service/impl/VoucherOrderServiceImpl.java
@@ -9,6 +9,7 @@ import com.cjun.service.IVoucherOrderService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.cjun.utils.RedisIdWorker;
 import com.cjun.utils.UserHolder;
+import org.springframework.aop.framework.AopContext;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -35,7 +36,6 @@ public class VoucherOrderServiceImpl extends ServiceImpl<VoucherOrderMapper, Vou
     @Resource IVoucherOrderService voucherOrderService;
 
     @Override
-    @Transactional
     public Result seckillVoucher(Long voucherId) {
         //1. 查询优惠券
         SeckillVoucher voucher= seckillVoucherService.getById(voucherId);
@@ -53,7 +53,30 @@ public class VoucherOrderServiceImpl extends ServiceImpl<VoucherOrderMapper, Vou
         if (voucher.getStock() < 1) {
             return Result.fail("库存不足! ");
         }
-        //5. 扣减库存
+        //5. 一人一单
+        Long userId = UserHolder.getUser().getId();
+        synchronized (userId.toString().intern()) {
+            IVoucherOrderService proxy = (IVoucherOrderService) AopContext.currentProxy();
+            //return this.createVoucherOrder(voucherId);
+            return proxy.createVoucherOrder(userId, voucherId);
+        }
+    }
+
+    @Transactional
+    @Override
+    public Result createVoucherOrder(Long userId, Long voucherId) {
+        //5.1 查询订单
+        long count = query()
+                .eq("user_id", userId)
+                .eq("voucher_id", voucherId)
+                .count();
+        //5.2 判断是否存在
+        if (count > 0) {
+            //用户已经购买过了
+            return Result.fail("用户已经购买过一次! ");
+        }
+
+        //6. 扣减库存
         boolean success = seckillVoucherService.update()
                 .setSql("stock = stock - 1")
                 .eq("voucher_id", voucherId)
@@ -63,18 +86,20 @@ public class VoucherOrderServiceImpl extends ServiceImpl<VoucherOrderMapper, Vou
         if (!success) {
             return Result.fail("扣减库存失败");
         }
-        //6. 创建订单
+
+
+        //7. 创建订单
         VoucherOrder voucherOrder = new VoucherOrder();
-        //6.1 订单id
+        //7.1 订单id
         long orderId = redisIdWorker.nextId("order");
         voucherOrder.setId(orderId);
-        //6.2 用户id
-        Long userId = UserHolder.getUser().getId();
+        //7.2 用户id
+        //Long userId = UserHolder.getUser().getId();
         voucherOrder.setUserId(userId);
-        //6.3 代金券
+        //7.3 代金券
         voucherOrder.setVoucherId(voucherId);
         voucherOrderService.save(voucherOrder);
-        //7. 返回订单id
+        //8. 返回订单id
         return Result.ok(orderId);
     }
 }
